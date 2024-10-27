15770b88a793b29f9a8188dda20e1bb5b04cf327
Souei01 <mossesramos68@gmail.com>
2024-10-27 21:31:16 +0800

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        clash: ['Clash Display', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'], // Assuming Poppins is also being used
+      },
+    },
+  },
+  plugins: [],
+};

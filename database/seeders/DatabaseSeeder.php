80f90ebbb1c77996fcabe3675737d2681716bce8
Art Art <144819984+lvl100boss@users.noreply.github.com>
2024-11-29 07:16:30 +0800

    greatly improve the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             DefaultUserSeeder::class,
             DefaultShopSeeder::class,
             DefaultShopBranchSeeder::class,
+            ShopImageSeeder::class,
         ]);
     }
 }

1ec3f7198f6ee64495246aecdecd62697e17eb19
Art Art <144819984+lvl100boss@users.noreply.github.com>
2024-12-03 00:19:56 +0800

    minor fixed
    sidebar behaviour fix, and admin update

--- resources/views/components/user-layout.blade.php
+++ resources/views/components/user-layout.blade.php
@@ -14,12 +14,12 @@
 <body>
     {{-- Header --}}
 
-    <x-loading-screen />
-    <div class="bg-milk min-h-screen relative scroll-smooth pb-20 z-0 pt-[3rem] lg:pt-[6rem] overflow-hidden">
+    {{-- <x-loading-screen /> --}}
+    <div class="bg-milk min-h-screen relative scroll-smooth pb-20 z-0 pt-[3rem] lg:pt-[6rem]">
         <x-user-header :user="$user" :userrole="$userrole" />
         {{-- SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR SIDEBAR --}}
         <div
-            class="flex mx-4 justify-center lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl lg:mx-auto gap-10 ">
+            class="flex mx-4 justify-center lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl lg:mx-auto gap-10">
             <x-user-sidebar />
             <div id="content" class="flex-1 min-w-0">
                 {{ $slot }}
--- resources/views/components/user-sidebar.blade.php
+++ resources/views/components/user-sidebar.blade.php
@@ -1,8 +1,6 @@
 <section class="hidden lg:block" id="sidebar-container">
-    {{-- <div id="user-sidebar"
-        class="fixed lg:sticky shadow-lg w-64 lg:w-52 px-4 lg:px-0 lg:pr-2 pb-8 top-16 pt-10 lg:pt-0 lg:top-28 gap-3 flex flex-col overflow-y-auto bg-neutral-100 lg:bg-transparent lg:shadow-none z-50"> --}}
     <div id="user-sidebar"
-        class="fixed lg:sticky shadow-lg w-64 lg:w-52 px-4 lg:px-0 lg:pr-2 pb-8 top-16 pt-10 lg:pt-0  gap-3 flex flex-col overflow-y-auto bg-neutral-100 lg:bg-transparent lg:shadow-none z-50">
+        class="fixed lg:sticky shadow-lg w-64 lg:w-52 px-4 lg:px-0 lg:pr-2 pb-8 top-16 pt-10 lg:pt-0 lg:top-28 gap-3 flex flex-col overflow-y-auto bg-neutral-100 lg:bg-transparent lg:shadow-none z-50">
         {{-- Home --}}
 
         <a href="{{ route('index') }}" id="home-link" data-url="{{-- route('home.content') --}}"

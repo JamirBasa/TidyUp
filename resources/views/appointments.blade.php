b91f77cb04861336ad4c494c745f17890b971b8a
lvl100boss <mc1791798@gmail.com>
2025-03-19 03:54:19 +0800

    asdasd

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -109,17 +109,63 @@
                     </div>
                 @endforeach
             @endif
-            
+
         </div>
         <div class="flex my-6 justify-end w-full">
-            <button class="flex items-center gap-2" id="cancel-appointments-button">
-                <svg class="stroke-1 stroke-black" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9.99902 16H4.99902V21M13.999 8H18.999V3M4.58203 9.0034C5.14272 7.61566 6.08146 6.41304 7.29157 5.53223C8.50169 4.65141 9.93588 4.12752 11.4288 4.02051C12.9217 3.9135 14.4138 4.2274 15.7371 4.92661C17.0605 5.62582 18.1603 6.68254 18.9131 7.97612M19.4166 14.9971C18.8559 16.3848 17.9171 17.5874 16.707 18.4682C15.4969 19.3491 14.0642 19.8723 12.5713 19.9793C11.0784 20.0863 9.58509 19.7725 8.26172 19.0732C6.93835 18.374 5.83785 17.3175 5.08496 16.0239"  stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+            <button class="flex items-center gap-2" id="open-cancel-appointments" onclick="openCancelPopup()">
+                <svg class="stroke-1 stroke-black" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9.99902 16H4.99902V21M13.999 8H18.999V3M4.58203 9.0034C5.14272 7.61566 6.08146 6.41304 7.29157 5.53223C8.50169 4.65141 9.93588 4.12752 11.4288 4.02051C12.9217 3.9135 14.4138 4.2274 15.7371 4.92661C17.0605 5.62582 18.1603 6.68254 18.9131 7.97612M19.4166 14.9971C18.8559 16.3848 17.9171 17.5874 16.707 18.4682C15.4969 19.3491 14.0642 19.8723 12.5713 19.9793C11.0784 20.0863 9.58509 19.7725 8.26172 19.0732C6.93835 18.374 5.83785 17.3175 5.08496 16.0239"
+                        stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
                 <span>Cancel Appointment Request</span>
             </button>
         </div>
+        <div id="cancel-popup"
+            class=" fixed top-0 right-0 bottom-0 left-0 backdrop-brightness-50 hidden place-items-center z-[3000]">
+            <div class="bg-white p-10 rounded-lg w-[40rem]">
+                <div class="flex items-start justify-between mb-6">
+                    <h1 class="text-4xl font-bold">Cancel Appointment</h1>
+                    <button class="" onclick=" closeCancelPopup()">
+                        <svg class="stroke-2 stroke-black" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" >
+                            <path d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="mb-2">
+                    <h1 class="mb-2">Reason for cancellation of Appointment</h1>
+                    <form action="">
+                        <textarea class="border rounded-lg w-full p-3" name="reason" id="" cols="30" rows="5" placeholder="Put the reason of the cancellation"></textarea>
+                    </form>
+                </div>
+                <div class="mb-6">
+                    <p class="text-red-500 text-sm">Disclaimer: If the cancellation was made 24 hours before the scheduled appointment, sanction to the account will be enforced.</p>
+                </div>
+                <div class="w-full grid grid-cols-2 gap-3">
+                    <button class="bg-neutral-100 text-black p-3 font-bold rounded-lg" onclick="closeCancelPopup()">Cancel</button>
+                    <button class="bg-brand-500 text-white p-3 font-bold rounded-lg" onclick="closeCancelPopup()">Confirm</button>
+                </div>
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            function openCancelPopup() {
+                const openCancelPopup = document.getElementById('open-cancel-appointments');
+                const cancelPopup = document.getElementById('cancel-popup');
+                cancelPopup.classList.remove('hidden');
+                cancelPopup.classList.add('grid');
+            }
+
+            function closeCancelPopup() {
+                const openCancelPopup = document.getElementById('open-cancel-appointments');
+                const cancelPopup = document.getElementById('cancel-popup');
+                cancelPopup.classList.add('hidden');
+                cancelPopup.classList.remove('grid');
+            }
+        </script>
 
         <script>
             $(document).ready(function() {
@@ -154,10 +200,10 @@
                         <div class="border-y border-neutral-400 flex items-center justify-between py-4 px-2 mb-4">
                             <div class="flex items-center gap-8 ">
                                 ${appointment.status === 'pending' ? `
-                                <div class="grid place-items-center" id="">
-                                    <input type="checkbox" name="select" id="appointments-checkbox">
-                                </div>
-                                ` : ''}
+                                        <div class="grid place-items-center" id="">
+                                            <input type="checkbox" name="select" id="appointments-checkbox">
+                                        </div>
+                                        ` : ''}
                                 <img class="h-28 w-52 rounded-lg object-cover flex-shrink-0"
                                     src="{{ asset('storage/') }}/${appointment.path}" loading="lazy"
                                     alt="">
